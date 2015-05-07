2b8f00eb6c64abae64d30b413088d52537540109
Nathan <nathansherburn@gmail.com>
2015-05-07 10:31:18 +1000

    Add "Sortable" through bower

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var server = express();
 
-server.use("/public", express.static(__dirname + '/public'));
+server.use(express.static(__dirname + '/public'));
 
-server.get("/", function (req, res) { res.send("hello"); } )
+//server.get("/", function (req, res) { res.send("hello"); } )
 
 var port = process.env.PORT || 3000;
 server.listen(port)
