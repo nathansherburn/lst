1ca17e9b139dc3e6bc29f67ee257a57247da22db
Nathan <nathansherburn@gmail.com>
2015-05-07 10:15:02 +1000

    Update server (test)

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 var express = require('express');
 var server = express();
 
-server.use(express.static(__dirname + '/public'));
+server.use("/public", express.static(__dirname + '/public'));
 
-server.ger("/", function (req, res) { send("hello"); } )
+server.get("/", function (req, res) { res.send("hello"); } )
 
 server.listen(3000);
