eaedd78c4e324d9b72b90bdf832bb840ed1413db
Nathan <nathansherburn@gmail.com>
2015-05-11 13:00:49 +1000

    Cat testing

--- server.js
+++ server.js
@@ -4,8 +4,16 @@ var mongoose	= require('mongoose');
 
 server.use(express.static(__dirname + '/public'));
 
+mongoose.connect('mongodb://<dbuser>:<dbpassword>@ds037067.mongolab.com:37067/heroku_app36733834');
+
 server.get("/items", function (req, res) {
-	res.json("hello");
+	var Cat = mongoose.model('Cat', { name: String });
+
+	var kitty = new Cat({ name: 'Zildjian' });
+	kitty.save(function (err) {
+	  if (err) return console.log(err)
+	  res.json("hello");
+	});
 });
 
 server.post("/items/add", function (req, res) {
