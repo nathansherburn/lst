f6f29ca599726c5237b01cf1611f2245e391f370
Nathan <nathansherburn@gmail.com>
2015-05-11 11:47:12 +1000

    Huge feature add

--- public/app.js
+++ public/app.js
@@ -1,40 +1,92 @@
 var lst = angular.module("lst",['ng-sortable'])
 
-lst.controller("MainCtrl", ['$scope', function ($scope) {
+lst.controller("MainCtrl", ['$scope', '$timeout', function ($scope, $timeout) {
 	$scope.items = [
 	{
+    priority: 1,
 		value:'learn Sortable',
 		created: new Date()
 	},
 	{
+    priority: null,
 		value:'use gn-sortable',
 		created: new Date()
 	},
 	{
+    priority: null,
 		value:'Enjoy',
 		created: new Date()
 	}
 	];
 
-	document.getElementById('new-item').onkeypress = function(e){
-    if (!e) e = window.event;
-    var keyCode = e.keyCode || e.which;
-    if (keyCode == '13'){
-      // Enter pressed
-      $scope.$apply(function () {
-      	$scope.items.push({value: $scope.newItem, created: new Date() });
-      });
-      console.log($scope.items)
-      this.blur();
-      $scope.$apply(function () {$scope.newItem = "";});
-      return false;
+  $scope.backlog = [
+  {
+    priority: null,
+    value:'test 1',
+    created: new Date()
+  },
+  {
+    priority: null,
+    value:'test barry',
+    created: new Date()
+  },
+  {
+    priority: null,
+    value:'selma',
+    created: new Date()
+  }
+  ];
+
+
+  $scope.backlogOpen = false;
+
+	$scope.addItem = function (clickEvent) {
+    console.log(clickEvent)
+    if (clickEvent.keyCode === 13)
+    {
+      $scope.items.push({value: $scope.newItem, created: new Date() });
+      // post to server
+      $scope.newItem = "";
+      
+      // collapse mobile keyboard
+      clickEvent.target.blur()
     }
-   
-   $scope.done = function () {
-   	console.log('hehe');
-   }
   }
 
+  $scope.deleteItem = function (id) {
+    // delete item from server
+
+    // delete item from list
+    console.log("pretending to delete " + id)
+  }
+   
+   $scope.done = function () {
+    // delete from server
+
+    // remove from list
+    console.log(this.item)
+    var index = $scope.items.indexOf(this.item);
+    $scope.items.splice(index, 1);     
+  }
+
+
+  $scope.moveFromBacklogToList = function () {
+    // add to items list
+    $scope.items.push(this.item);
+
+    // remove from backlog
+    var index = $scope.backlog.indexOf(this.item);
+    $scope.backlog.splice(index, 1);     
+  }
+
+  $scope.moveFromListToBacklog = function () {
+    // add to backlog
+    $scope.backlog.push(this.item);
+
+    // remove from items
+    var index = $scope.items.indexOf(this.item);
+    $scope.items.splice(index, 1);     
+  }
 
 
 }]);
--- server.js
+++ server.js
@@ -1,9 +1,21 @@
-var express = require('express');
-var server = express();
+var express 	= require('express');
+var server 		= express();
+var mongoose	= require('mongoose');
 
 server.use(express.static(__dirname + '/public'));
 
-//server.get("/", function (req, res) { res.send("hello"); } )
+server.get("/items", function (req, res) {
+	res.json("hello");
+});
+
+server.post("/items/add", function (req, res) {
+	res.json("hello");
+});
+
+server.post("/items/delete", function (req, res) {
+	res.json("hello");
+});
+
 
 var port = process.env.PORT || 3000;
 server.listen(port)
